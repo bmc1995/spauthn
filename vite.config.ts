cc01894d4d20fd10a900d0dac9cccf2239a42d60
Billy McCrackin <44419353+bmc1995@users.noreply.github.com>
2025-06-23 18:33:24 -0600

    chore(config): update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 4200, // Change this to your desired port
+  },
 });
 
 /* https://vitest.dev/config/
